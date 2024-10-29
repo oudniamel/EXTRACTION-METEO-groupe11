035341d7c1952f247ac87fba057dd91a0861f8b0
Nathan <nathanbarraud05@gmail.com>
2024-10-29 14:28:54 +0100

    Variante 1

--- Extracteur_Meteo.sh
+++ Extracteur_Meteo.sh
@@ -1,8 +1,6 @@
-
-
 #!/bin/bash
 
-# Ville par defaut si aucun argument n'est fourni
+# Ville par défaut si aucun argument n'est fourni
 ville_defaut="Paris"
 
 # Vérification des arguments
@@ -11,12 +9,6 @@ if [ $# -eq 0 ]; then
 else
     nom_ville=$1
 fi
-
-#!/bin/bash
-
-echo "Veuillez entrer le nom d'une ville :"
-read nom_ville
->>>>>>> c81cc18 (Version1 : Ajout du script de base et de la documentation)
 
 # Récupération de la météo actuelle
 temperature_aujourdhui=$(curl -s "wttr.in/$nom_ville?format=%t")
@@ -28,23 +20,8 @@ temperature_demain=$(curl -s "wttr.in/$nom_ville?format=%t&tomorrow")
 jour_actuel=$(date '+%Y-%m-%d')
 heure_actuelle=$(date '+%H:%M')
 
-
-
-# Enregistrement des informations dans un fichier
-echo "$jour_actuel - $heure_actuelle - $nom_ville : $temperature_aujourdhui - $temperature_demain" >> previsions_meteo.txt
-
 # Création du fichier journalier avec la date (format : meteo_YYYYMMDD.txt)
 fichier_historique="meteo_$(date '+%Y%m%d').txt"
 
 # Enregistrement des informations dans le fichier journalier
 echo "$jour_actuel - $heure_actuelle - $nom_ville : $temperature_aujourdhui - $temperature_demain" >> "$fichier_historique"
-#!/bin/bash
-CITY=${1:-"Toulouse"} 
-DATE=$(date '+%Y-%m-%d - %H:%M')
-METEO_DATA=$(curl -s "wttr.in/$CITY?format=%t+%T")
-echo "$DATE - $CITY : $METEO_DATA" >> meteo.txt
-
-
-# Enregistrement des informations dans un fichier
-echo "$jour_actuel - $heure_actuelle - $nom_ville : $temperature_aujourdhui - $temperature_demain" >> previsions_meteo.txt
->>>>>>> c81cc18 (Version1 : Ajout du script de base et de la documentation)
